4d7eeecc137f1949c3384f6168a936a6621a2a08
Kimjaewoo <kimjaewoo@hufs.ac.kr>
2024-05-27 04:32:05 +0900

    가격 필터 최소 최대값 입력 박스 기능 구현

--- src/pages/main/components/filter/price/MinBoxPrice.js
+++ src/pages/main/components/filter/price/MinBoxPrice.js
@@ -1,19 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const MinBoxPrice = () => {
+    const [value, setValue] = useState('');
 
-    return(
+    const handleChange = (event) => {
+        const newValue = event.target.value === '' ? '' : Math.max(0, Math.min(50000, Number(event.target.value)));
+        setValue(newValue);
+    };
+
+    return (
         <Container>
-            <MinBox/>
+            <MinBox>
+                <StyledInput
+                    type="number"
+                    value={value}
+                    onChange={handleChange}
+                    min="0"
+                    max="50000"
+                    placeholder="인당"
+                />
+            </MinBox>
         </Container>
     );
-
 };
 
 const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
-
 
 const MinBox = styled.div`
     width: 7.5rem;
@@ -25,6 +41,32 @@ const MinBox = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-`
+`;
 
-export default MinBoxPrice
+const StyledInput = styled.input`
+    width: 100%;
+    height: 100%;
+    border: none;
+    text-align: center;
+    font-size: 1rem;
+    border-radius: 0.625rem;
+
+    /* Hide the spin buttons */
+    -moz-appearance: textfield;
+    &::-webkit-outer-spin-button,
+    &::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+
+    &::placeholder {
+        color: #d3d3d3; /* 연한 회색 */
+        text-align: center;
+    }
+
+    &:focus {
+        outline: none;
+    }
+`;
+
+export default MinBoxPrice;
