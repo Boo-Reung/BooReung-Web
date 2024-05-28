2b3caaee7355404dfae90763fb82ed079726835a
y-hyun2 <hyma0214@gmail.com>
2024-05-29 04:22:52 +0900

    현이 첫 API연동 성공

--- src/pages/after/components/InfoInputForm.jsx
+++ src/pages/after/components/InfoInputForm.jsx
@@ -4,35 +4,89 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 
-const InfoInputForm = () => {
+const InfoInputForm = ({nameChange, stuNumChange, phoneNumChange, postNumChange, hostNameChange, hostPhoneNumChange, carInfoChange, selectedDateChange}) => {
     const [selectedDate, setSelectedDate] =useState(null);
+    const total = (date) => {
+        selectedDateChange(date);
+        setSelectedDate(date)
+    };
+
+        const[userName, setUserName] = useState(null);
+        const userNameClick = (event) => {
+            setUserName(event.target.value);
+            nameChange(event.target.value);
+        };
+
+        const[stuNum, setStuNum] = useState(null);
+        const stuNameClick = (event) => {
+          setStuNum(event.target.value);
+          stuNumChange(event.target.value);
+        };
+    
+        const[phoneNum, setPhoneNum] = useState(null);
+        const phoneNumClick = (event) => {
+              setPhoneNum(event.target.value);
+              phoneNumChange(event.target.value);
+            };
+        
+        const[postNum, setPostNum] = useState(null);
+        const postNumClick = (event) => {
+              setPostNum(event.target.value);
+              postNumChange(event.target.value);
+            };
+
+        const[hostName, setHostName] = useState(null);
+        const hostNameClick = (event) => {
+                setHostName(event.target.value);
+                hostNameChange(event.target.value);
+            };
+        
+        const[hostPhoneNum, setHostPhoneNum] = useState(null);
+        const hostPhoneNumClick = (event) => {
+              setHostPhoneNum(event.target.value);
+              hostPhoneNumChange(event.target.value);
+            };
+    
+        const[carInfo, setCarInfo] = useState(null);
+        const carInfoClick = (event) => {
+              setCarInfo(event.target.value);
+              carInfoChange(event.target.value);
+            };
+            
     return(
         <FormContainer>
             <Section>
             <SectionTitle>본인 정보 입력</SectionTitle>
             <Label>이름을 입력하세요</Label>
-            <Input type="text" placeholder="5글자 이내로 입력하세요" />
+            <Input type="text" value={userName} onChange={userNameClick}
+            placeholder="5글자 이내로 입력하세요" />
             <Label>학번을 입력하세요</Label>
-            <Input type = "text" placeholder = "201900000"/>
+            <Input type = "text" value={stuNum} onChange = {stuNameClick}
+            placeholder = "201900000"/>
             <Label>전화번호를 입력하세요</Label>
-            <Input type = "text" placeholder = "ex) 01012345678"/>
+            <Input type = "text" value = { phoneNum} onChange={phoneNumClick}
+            placeholder = "ex) 01012345678"/>
             </Section>
             
             <Section>
             <SectionTitle>카풀 정보 입력</SectionTitle>
             <Label>카풀 게시글 번호를 입력하세요</Label>
-            <Input type="text" placeholder="ex) 1004" />
+            <Input type="text" value={postNum} onChange={postNumClick}
+            placeholder="ex) 1004" />
             <Label>카풀 주최자 이름을 입력하세요</Label>
-            <Input type = "text" placeholder = "5글자 이내로 입력하세요"/>
+            <Input type = "text" value={hostName} onChange={hostNameClick}
+            placeholder = "5글자 이내로 입력하세요"/>
             <Label>카풀 주최자 전화번호를 입력하세요</Label>
-            <Input type = "text" placeholder = "ex) 01012345678"/>
+            <Input type = "text" value={hostPhoneNum} onChange={hostPhoneNumClick}
+            placeholder = "ex) 01012345678"/>
             <Label>차량 정보</Label>
-            <Input type = "text" placeholder = "차량의 색, 차종, 번호판을 입력하세요"/>
+            <Input type = "text" value={carInfo} onChange={carInfoClick}
+            placeholder = "차량의 색, 차종, 번호판을 입력하세요"/>
             <Label>카풀 한 날짜와 시간을 입력하세요</Label>
             <DateWrapper>
                 <StyledDate
                     selected={selectedDate}
-                    onChange={(date) => setSelectedDate(date)}
+                    onChange={(date) => total(date)}
                     showTimeSelect
                     dateFormat="Pp"
                     placeholderText="날짜와 시간 선택하기! (클릭)"
